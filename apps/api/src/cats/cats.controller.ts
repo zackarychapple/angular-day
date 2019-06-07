bd01d2149598e9c6f9f0d7e1692d00261ae73a98
zchapple <zackarychapple30@gmail.com>
2019-06-07 07:57:31 -0400

    working on simplifying controller

--- apps/api/src/cats/cats.controller.ts
+++ apps/api/src/cats/cats.controller.ts
@@ -1,37 +1,23 @@
 import {Body, Catch, Controller, Get, HttpException, HttpStatus, Param, Post, Res} from '@nestjs/common';
 import {Response} from 'express';
-import * as fs from "fs";
-import * as path from "path";
-import * as https from 'https';
 import {CatDto} from './cat.dto';
 import {CatsService} from './cats.service';
-import {BreedSearchDto} from './breed_search.dto';
 
 @Catch()
 @Controller('cats')
 export class CatsController {
-  allBreeds: Array<any>;
 
   constructor(private catsService: CatsService) {
-    this.allBreeds = []
   }
 
   @Get('/')
-  async getAllBreeds(@Res() res: Response) {
-    if (this.allBreeds.length === 0) {
-      // Originally from https://api.thecatapi.com/v1/breeds
-      const catsRaw = await fs.readFileSync(path.resolve(process.cwd(), `apps/api/src/cats/cats.json`));
-      const catsJson = JSON.parse(catsRaw.toString());
-      this.allBreeds = catsJson;
-      res.json(catsJson)
-    } else {
-      res.send(this.allBreeds)
-    }
+  async getAllBreeds() {
+    return await this.catsService.getAllBreeds();
   }
 
   @Post('/')
   async saveCat(@Body() catDto: CatDto) {
-    const saveResult = await this.catsService.saveCatByOwner(catDto);
+    const saveResult = this.catsService.saveCatByOwner(catDto);
     if (saveResult) {
       return {message: 'Cat Saved Successfully'}
     } else {
@@ -40,30 +26,8 @@ export class CatsController {
   }
 
   @Get('/:breedID')
-  getBreed(@Param('breedID') breedID: string, @Res() res: Response) {
-    const breedQuery =
-      `https://api.thecatapi.com/v1/images/search?limit=9&mime_types=&order=Random&size=small&page=0&breed_ids=${breedID}&sub_id=demo-a44f13`;
+  async getBreed(@Param('breedID') breedID: string, @Res() res: Response) {
 
-    https.get(breedQuery, (response: any) => {
-
-        let json = '';
-        response.on('data', function (chunk) {
-          json += chunk;
-        });
-        response.on('end', function () {
-          if (res.statusCode === 200) {
-            try {
-              const data: Array<BreedSearchDto> = JSON.parse(json);
-              res.status(HttpStatus.OK);
-              res.json(data);
-            } catch (e) {
-              res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({error: 'Error Parsing Cats'});
-            }
-          } else {
-            res.status(HttpStatus.NOT_FOUND).json({error: 'Breed Not Found'});
-          }
-        });
-      }
-    );
+    return await this.catsService.getCatsByBreed(breedID, res);
   }
 }
--- apps/api/src/cats/cats.service.ts
+++ apps/api/src/cats/cats.service.ts
@@ -3,12 +3,62 @@ import {CatDto} from './cat.dto';
 import {CatEntity} from './cat.entity';
 import {Repository} from 'typeorm';
 import {InjectRepository} from '@nestjs/typeorm';
+import * as fs from "fs";
+import * as path from "path";
+import {BreedDto} from './breed.dto';
+import * as https from "https";
+import {Response} from 'express';
+import {BreedSearchDto} from './breed_search.dto';
 
 @Injectable()
 export class CatsService {
+  allBreeds: Array<BreedDto>;
+
   constructor(@InjectRepository(CatEntity)
               private readonly catEntity: Repository<CatEntity>) {
+    this.allBreeds = []
+  }
 
+  async getAllBreeds(): Promise<Array<BreedDto>> {
+    if (this.allBreeds.length === 0) {
+      // Originally from https://api.thecatapi.com/v1/breeds
+      const catsRaw = await fs.readFileSync(path.resolve(process.cwd(), `apps/api/src/cats/cats.json`));
+      const catsJson = JSON.parse(catsRaw.toString());
+      this.allBreeds = catsJson;
+      return catsJson
+    } else {
+      return this.allBreeds
+    }
+  }
+
+  // TODO: Add Redis Cache and Memory cache of the responses from the breeds
+  // TODO: I dont like that I have to pass in the response
+  async getCatsByBreed(breedID: string, res: Response) {
+    const breedQuery =
+      `https://api.thecatapi.com/v1/images/search?limit=9&mime_types=&order=Random&size=small&page=0&breed_ids=${breedID}&sub_id=demo-a44f13`;
+    https.get(breedQuery, (response: any) => {
+
+        let json = '';
+        response.on('data', function (chunk) {
+          json += chunk;
+        });
+        response.on('end', function () {
+          // TODO: Add this handling back
+          // if (response.statusCode === 200) {
+          try {
+            const data: Array<BreedSearchDto> = JSON.parse(json);
+            res.json(data);
+          } catch (e) {
+            debugger
+            //   TODO: This crashes the server
+            // throw new HttpException({error: 'Error Parsing Cats'}, HttpStatus.INTERNAL_SERVER_ERROR)
+            // }
+            // } else {
+            //   throw new HttpException({error: 'Breed Not Found'}, HttpStatus.NOT_FOUND)
+          }
+        });
+      }
+    );
   }
 
   async getCatsByOwner(onwerName: string): Promise<any> {
